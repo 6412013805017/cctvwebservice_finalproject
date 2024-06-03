23b245f8856535b39ab54ee05fa48f4459147f2a
6412013805017 <167610007+6412013805017@users.noreply.github.com>
2024-06-03 21:10:01 +0700

    update

--- template/index.php
+++ template/index.php
@@ -1,3 +1,38 @@
+<?php
+    require_once("./action/dbconnect.php");
+
+    try {
+        $stmt = $con->prepare("
+            SELECT 
+                u.*,
+                COUNT(DISTINCT w.worksite_id) AS num_worksites,
+                COUNT(DISTINCT n.noti_id) AS num_services,
+                MAX(s.service_datetime) AS latest_service_date
+            FROM 
+                user u
+            JOIN 
+                account a ON u.user_id = a.user_id
+            LEFT JOIN 
+                worksite w ON u.user_id = w.user_id
+            LEFT JOIN 
+                notification n ON u.user_id = n.user_id AND n.noti_status = 0
+            LEFT JOIN 
+                service s ON s.noti_id = n.noti_id
+            WHERE 
+                a.account_type = 'customer'
+            GROUP BY 
+                u.user_id;
+        ");
+        $stmt->execute();
+    
+        $customers = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    } catch(PDOException $e) {
+        echo "Connection failed: " . $e->getMessage();
+    }
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
